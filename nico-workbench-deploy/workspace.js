c96d89b83d221e80470a9979ceb5fe937c58811a
Nicholas112302 <nicholasleeyongjin@gmail.com>
2026-08-26 15:17:26 +0800

    fix: remove BD workspace references from media shell

--- nico-workbench-deploy/workspace.js
+++ nico-workbench-deploy/workspace.js
@@ -1,6 +1,6 @@
 (()=>{
   try{localStorage.removeItem('nico_active_workspace')}catch(e){}
-  document.querySelectorAll('.workspace-switcher,.workspace-bd').forEach(el=>el.remove());
+  document.querySelectorAll('.workspace-switcher').forEach(el=>el.remove());
   document.querySelectorAll('.workspace-media-hidden').forEach(el=>el.classList.remove('workspace-media-hidden'));
   document.title='Nico Workbench';
 })();
